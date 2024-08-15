af2ced81007b9933321a5644ddeb85e3a0ddad00
Bhagavath-2004 <139193428+Bhagavath-2004@users.noreply.github.com>
2024-08-15 12:32:08 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,15 @@
+jsx
+  import React from 'react';
+
+  const About = () => {
+    return (
+      <section className="text-center text-white bg-black py-20">
+        <h2 className="text-4xl font-bold mb-4">About Us</h2>
+        <p className="max-w-2xl mx-auto">
+          We are a company created 2 years ago which loves to share amazing minimalistic inspiration with the world. Everything we share will help you get inspired weekly.
+        </p>
+      </section>
+    );
+  };
+
+  export default About;
